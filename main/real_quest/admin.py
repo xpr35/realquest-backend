a9ffdb64139ee6ee8646c4d7b3ac107c3f27e544
Lev Novokreshchenov <xpr35@live.com>
2017-04-15 17:08:41 +0600

    add model

--- main/real_quest/admin.py
+++ main/real_quest/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+class UserModelAdmin(admin.ModelAdmin):
+    pass
+
+class QuestStagesAdmin(admin.ModelAdmin):
+    pass
+
+class QuestModelAdmin(admin.ModelAdmin):
+    pass
+
+class PartyModelAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(UserModel, UserModelAdmin)
+admin.site.register(QuestStages, QuestModelAdmin)
+admin.site.register(QuestModel, QuestModelAdmin)
+admin.site.register(PartyModel, PartyModelAdmin)
--- main/real_quest/models.py
+++ main/real_quest/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class UserModel(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="auth_model")
+    balance = models.FloatField()
+
+
+class QuestStages(models.Model):
+    description = models.CharField(max_length=255)
+    status = models.CharField(max_length=255,
+                              choices=[("Waiting", "waiting"), ("In Progress", "in_progress"), ("Done", "done")])
+    cost = models.FloatField()  # ToDO currencies
+    proof = models.FileField()
+
+
+class QuestModel(models.Model):
+    producer = models.OneToOneField(UserModel, on_delete=models.SET_NULL, null=True, related_name="producer_model")
+    consumer = models.OneToOneField(UserModel, on_delete=models.SET_NULL, null=True, related_name="consumer_model")
+    create_date = models.DateTimeField()
+    end_date = models.DateTimeField()
+    stages = models.ForeignKey(QuestStages)
+    tags = models.CharField(max_length=30)
+
+
+class PartyModel(models.Model):
+    users = models.ForeignKey(UserModel)
+    status = models.CharField(max_length=255, choices=[("Empty", "empty"), ("Not Full", "not_full"), ("Full", "full")])
+    leader = models.OneToOneField(UserModel, on_delete=models.SET_NULL, null=True, related_name="leader_model")
