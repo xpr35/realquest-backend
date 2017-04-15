a6b7507b6a113a1d77a8c64f47afcab6d3926301
Lev Novokreshchenov <xpr35@live.com>
2017-04-15 21:47:27 +0600

    add serializer, views and router

--- main/main/urls.py
+++ main/main/urls.py
@@ -13,9 +13,18 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
+from rest_framework import routers
+from real_quest.views import *
+from rest_framework.authtoken import views as ww
 
+router = routers.DefaultRouter()
+router.register(r'profiles', UserModelViewSet, base_name="users")
+# urlpatterns = router.urls
 urlpatterns = [
+    url(r'^api/', include(router.urls)),
     url(r'^admin/', admin.site.urls),
+    url(r'^api-token-auth/', ww.obtain_auth_token),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
--- main/real_quest/views.py
+++ main/real_quest/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import *
+from .serializers import *
+from rest_framework.authentication import TokenAuthentication
 
-# Create your views here.
+
+class UserModelViewSet(viewsets.ModelViewSet):
+    queryset = UserModel.objects.all()
+    serializer_class = UserModelSerializer
+    permission_classes = [TokenAuthentication]
+
+
+class QuestStagesViewSet(viewsets.ModelViewSet):
+    queryset = QuestStages.objects.all()
+    serializer_class = QuestStagesSerializer
+    permission_classes = [TokenAuthentication]
+
+
+class QuestModelViewSet(viewsets.ModelViewSet):
+    queryset = QuestModel.objects.all()
+    serializer_class = QuestModelSerializer
+    permission_classes = [TokenAuthentication]
+
+
+class PartyModelViewSet(viewsets.ModelViewSet):
+    queryset = PartyModel.objects.all()
+    serializer_class = PartyModelSerializer
+    permission_classes = [TokenAuthentication]
